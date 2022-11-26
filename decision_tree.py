a08eee9901478c7dd17095a19ef869d973a27c4c
hamid_rd3 <h.dashtabadi@gmail.com>
2022-11-26 18:54:32 +0330

    get best feature  has been implemented

--- decision_tree.py
+++ decision_tree.py
@@ -15,19 +15,36 @@ def  get_entropy(df):
 def getIG(df,feature):
     
     size = df.shape[0]
-    value_count=df[feature].value_counts()
+    colnum = df.size/df.shape[0]
+    if colnum != 1:
+        value_count=df[feature].value_counts()
+    else :
+        value_count=df
     avg_info=sum([value_count[val] * get_entropy(df[df[feature] == val])
          for val in df[feature].unique()])
     return get_entropy(df)-avg_info/size   
 
+def getbestfeat(df):
+    IGfeatures=[]
+    colnum = df.size/df.shape[0]
+    if colnum!=1 :
+        for feature in df.columns :
+            IGfeatures.append(getIG(df,feature))
+    elif colnum==1 :
+        return None 
+    return max(df.columns[:-1], key=lambda feature: getIG(df, feature))
+
+    # for feature in df
 #part a
-df = pd.read_csv('nursery.csv')
-df.dropna()
-train = df.sample(frac=0.8, random_state=200)
-test = df.drop(train.index)
+df1 = pd.read_csv('nursery.csv')
+df1.dropna()
+train = df1.sample(frac=0.8, random_state=200)
+test = df1.drop(train.index)
 
 # print (train.shape,test.shape)
-# print(df[0:2].size/df[0:2].shape[0])
-# print(df['final evaluation'].value_counts()['not_recom'])
-# print([df[df['final evaluation']==x] for x in df['final evaluation'].value_counts()])
-# print(getIG(df, 'final evaluation'), getIG(df, 'health'))
+# print(df1[0:2].size/df1[0:2].shape[0])
+# print(df1['final evaluation'].value_counts()['not_recom'])
+# print([df1[df1['final evaluation']==x] for x in df1['final evaluation'].value_counts()])
+# print(getIG(df1, 'final evaluation'), getIG(df1, 'health'))
+print(getbestfeat(df1['final evaluation']))
+

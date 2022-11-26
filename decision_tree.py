f06bfaddcbb884bb494d7fc089985cdd2e0a4f2d
hamid_rd3 <h.dashtabadi@gmail.com>
2022-11-26 18:10:25 +0330

    get_IG implemented

--- decision_tree.py
+++ decision_tree.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 
 def  get_entropy(df):
+    
     colnum = df.size/df.shape[0]
     size=df.shape[0]
     if colnum!=1:
@@ -10,6 +11,14 @@ def  get_entropy(df):
     elif colnum==1:
         # labels column
         return sum([-x/size*np.log2(x/size) for x in df.value_counts() ])
+
+def getIG(df,feature):
+    
+    size = df.shape[0]
+    value_count=df[feature].value_counts()
+    avg_info=sum([value_count[val] * get_entropy(df[df[feature] == val])
+         for val in df[feature].unique()])
+    return get_entropy(df)-avg_info/size   
 
 #part a
 df = pd.read_csv('nursery.csv')
@@ -19,5 +28,6 @@ test = df.drop(train.index)
 
 # print (train.shape,test.shape)
 # print(df[0:2].size/df[0:2].shape[0])
-# print(df[0:2].value_counts)
-print(get_entropy(df[:500]))
+# print(df['final evaluation'].value_counts()['not_recom'])
+# print([df[df['final evaluation']==x] for x in df['final evaluation'].value_counts()])
+# print(getIG(df, 'final evaluation'), getIG(df, 'health'))

062fed5b7cf3d0c336ae64913cd51f93d20fbe11
hamid_rd3 <h.dashtabadi@gmail.com>
2022-12-01 23:56:18 +0330

    final result

--- decision_tree.py
+++ decision_tree.py
@@ -7,6 +7,8 @@ def get_entropy(df):
     # max entropy is log2 (c) where c is amount of features
     colnum = df.size/df.shape[0]
     size = df.shape[0]
+    #could not to pick up last column of an one column dataframe  ...
+    # -> df[df.columns[-1]] vs df
     if colnum != 1:
         return sum([-x/size*np.log2(x/size) for x in df[df.columns[-1]].value_counts()])
     elif colnum == 1:
@@ -36,6 +38,7 @@ def getIG(df, feature, criterion='entropy'):
     else:
         value_count = df
     if criterion == 'gini':
+        #select branch dataframe and calculate average information
         avg_info = sum([value_count[val] * get_gini(df[df[feature] == val])
                         for val in df[feature].unique()])
         return get_gini(df)-avg_info/size
@@ -46,6 +49,7 @@ def getIG(df, feature, criterion='entropy'):
 
 
 def getbestfeat(df, criterion='entropy'):
+    '''return best feature as the biggest information gain '''
     colnum = df.size/df.shape[0]
     if colnum != 1:
         return max(df.columns[:-1], key=lambda feature: getIG(df, feature, criterion))
@@ -75,8 +79,11 @@ class tree:
         self.y_predict = None
 
     def fit(self):
+        ''' it makes decision tree based on ID3 algorithm '''
         if self.df.shape[1] == 2 or self.depth == 1:
+            #leaf Node or reachs maximum depth 
             labels = self.df.columns[-1]
+            #label data = max lable value 
             self.root = Node(max(self.df[
                 labels].unique(), key=lambda x: self.df[labels].value_counts()[x]))
             return self.root
@@ -86,9 +93,12 @@ class tree:
             residual_columns = [
                 k for k in self.df.columns if k != self.root.attribute]
 
+            #Iterate through brnaches of the root 
             for val in self.df[self.root.attribute].unique():
                 df_tmp = self.df[self.df[self.root.attribute]
                                  == val][residual_columns]
+                #creates temprory tree to be fitted and take the ...
+                #attribute of child node based on information gain
                 tree_tmp = tree(df_tmp, self.depth-1,
                                 criterion=self.critertion)
                 root_tmp = tree_tmp.fit()
@@ -96,6 +106,7 @@ class tree:
         return self.root
 
     def predict(self, X_tr, node=None):
+        '''it predicts X test based on the fitted tree and return predicted y '''
         if node is None:
             node = self.root
         if self.y_predict is None:
@@ -110,73 +121,11 @@ class tree:
             for b, c in node.branch_childs.items():
                 self.predict(X_tr[X_tr[node.attribute] == b], c)
         else:
+            #create y_predict rows from leaf attibute 
             self.y_predict.loc[X_tr.axes[0]] = node.attribute
         return self.y_predict
 
     def accuracy(self, ytest):
+        '''compate ytest and ypredict'''
         return round(sum(self.y_predict == ytest)/self.y_predict.shape[0], 2)
 
-    # for feature in df
-
-
-# part a
-df1 = pd.read_csv('nursery.csv')
-df1.dropna()
-train = df1.sample(frac=0.8, random_state=3)
-test = df1.drop(train.index)
-
-start = time.time()
-t = tree(df1, 8) # complete tree 
-t.fit()
-print(t.root)
-print(f"predicted y :\n {t.predict(test[test.columns[:-1]])} \n")
-print(f"accuracy is \n {t.accuracy(test[test.columns[-1]])*100} %")
-print(round(time.time()-start, 2))
-
-# part b 
-
-train = df1.sample(frac=0.50, random_state=3)
-test = df1.drop(train.index)
-start = time.time()
-t1 = tree(df1, 6)  # complete tree
-t1.fit()
-print(t1.root)
-print(f"predicted y :\n {t1.predict(test[test.columns[:-1]])} \n")
-print(f"accuracy is \n {t1.accuracy(test[test.columns[-1]])*100} %")
-print(round(time.time()-start, 2))
-t2 = tree(df1, 8)  # complete tree
-t2.fit()
-print(t2.root)
-print(f"predicted y :\n {t2.predict(test[test.columns[:-1]])} \n")
-print(f"accuracy is \n {t2.accuracy(test[test.columns[-1]])*100} %")
-
-# y=df1['final evaluation']
-# y[:]=None
-# y.loc[df1['health'][[1,300, 600, 900,12958]].axes[0]] = 'asd'
-# print(y)
-
-# print(df1.loc[[300,600,900]])
-# print(df1.head())
-# print (train.shape,test.shape)
-# print(df1[0:2].size/df1[0:2].shape[0])
-# print(df1['final evaluation'].value_counts())
-# print([df1[df1['final evaluation']==x] for x in df1['final evaluation'].value_counts()])
-# print(getIG(df1, 'final evaluation'), getIG(df1, 'health'))
-# print(getIG(df1, 'final evaluation', 'gini'), getIG(df1, 'health', 'gini'))
-
-# print(getbestfeat(df1), getbestfeat(df1,'gini'))
-
-# n=Node('health',{'x':Node('mamad',{'x':0}),'y':0,'z':'1'})
-
-# print(Node('health'),)
-# print(n)
-# print(df1[df1['health'] == 'priority' and df1['children'] == 1])
-# print(max(df1['final evaluation'].unique(),key=lambda x: df1['final evaluation'].value_counts()[x]))
-# print(df1['final evaluation'].name)
-# x.remove('health')
-# print(df1[x])
-# print(df1[['final evaluation','health']])
-# t = tree(df1[['form', 'social', 'health', 'final evaluation']], 8)
-
-# print(t.root.branch_childs['recommended'])
-# print(df1[['form','social']][:500]['form'][400:])
